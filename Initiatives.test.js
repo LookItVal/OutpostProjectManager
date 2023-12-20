1c3ec15c7d72dcc9ec1d87373635057a690405e0
QVC <qnncecil@gmail.com>
2023-12-20 11:30:17 -0600

    Begin staging unit tests that do not exist yet

---
+++ Initiatives.test.js
@@ -0,0 +1,9 @@
+// We shoudl eventually unit test but maybe thats a task for when we migrate to TS
+
+function testInitiativesConstructor() {
+  try {
+    var initiative = new Initiatives({name: 1});
+  } catch (e) {
+    assertEquals('Initiatives name must be a string.', e.message);
+  }
+}
