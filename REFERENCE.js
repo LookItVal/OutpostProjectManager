1fde60ee692037beda888daa175701825f3f9417
QVC <86853474+LookItVal@users.noreply.github.com>
2023-12-20 09:57:19 -0600

    initial commit

---
+++ REFERENCE.js
@@ -0,0 +1,24 @@
+// This is literally just used to reference urls and object info i may need later. it is never run by the server.
+
+/* URLs:
+Main Icon: https://outpostworldwide.com/wp-content/uploads/2023/05/cropped-Outpost-Icon.png
+Temp Icon: https://outpostworldwide.com/wp-content/uploads/2023/11/Outpost-Icon-Square-INV.png
+*/
+
+/* Click on event. this is the object it sends.
+
+ { hostApp: 'calendar',
+  userLocale: 'en',
+  userTimezone: { offSet: '-21600000', id: 'America/Chicago' },
+  userCountry: '',
+  clientPlatform: 'web',
+  commonEventObject: 
+   { hostApp: 'CALENDAR',
+     timeZone: { id: 'America/Chicago', offset: -21600000 },
+     platform: 'WEB',
+     userLocale: 'en' },
+  calendar: 
+   { capabilities: { canSeeAttendees: true, canSeeConferenceData: true },
+     calendarId: 'bq6sdnbv73rbe6dljpsfa04l40@group.calendar.google.com',
+     id: '64f3nhf6s2v3pmp3mg4uba8v89',
+     organizer: { email: 'bq6sdnbv73rbe6dljpsfa04l40@group.calendar.google.com' } } } */
