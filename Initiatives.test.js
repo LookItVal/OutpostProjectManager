d6d22cb07bf9737e795084383520c660b617a428
QVC <86853474+LookItVal@users.noreply.github.com>
2024-01-02 11:38:57 -0600

    Remove unit test for Initiatives constructor

--- Initiatives.test.js
+++
@@ -1,9 +0,0 @@
-// We should eventually unit test but maybe thats a task for when we migrate to TS
-
-function testInitiativesConstructor() {
-  try {
-    var initiative = new Initiatives({name: 1});
-  } catch (e) {
-    assertEquals('Initiatives name must be a string.', e.message);
-  }
-}
