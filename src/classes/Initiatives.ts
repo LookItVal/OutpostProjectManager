cfe9fd2fd363413aab90182c0cc80ec7e7e49452
QVC <qnncecil@gmail.com>
2024-01-02 17:04:07 -0600

    Add validation error class and constants file

--- src/classes/Initiatives.ts
+++ src/classes/Initiatives.ts
@@ -1,4 +1,5 @@
-import { ValidationError } from "../errors/errors";
+import { ValidationError } from "../errors";
+import { projectDataSheetId, regex4Digits } from "../constants";
 
 interface InitiativeParams {
   name?: string;
@@ -13,6 +14,7 @@ export class Initiative {
   protected _projectName?: string;
   protected _folder?: GoogleAppsScript.Drive.Folder;
   protected _folderId?: string;
+  protected _dataSpreadsheet?: GoogleAppsScript.Spreadsheet.Spreadsheet;
 
 
   constructor({ name = "", nameArray = [], folder = null }: InitiativeParams) {
@@ -36,5 +38,69 @@ export class Initiative {
       return;
     }
     throw new ValidationError("Initiative must be initialized with a name, nameArray, or folder");
+  }
+
+  /////////////////////////////////////////////
+  //                Properties               //
+  /////////////////////////////////////////////
+
+  public get dataSpreadsheet(): GoogleAppsScript.Spreadsheet.Spreadsheet {
+    if (this._dataSpreadsheet) {
+      return this._dataSpreadsheet;
+    }
+    this._dataSpreadsheet = SpreadsheetApp.openById(projectDataSheetId);
+    return this._dataSpreadsheet;
+  }
+
+  public get clientName() {
+    if (this._clientName) {
+      return this._clientName;
+    }
+    var clientProject: string = this.title.split(" ").slice(2).join(" ");
+    // TODO: this next line requires both the definition for the client class and also a getClients() function
+    // would i define the client to be the Client class?
+    // var allClients: string[] = getClients().map(client => client.name);
+    var allClients: string[] = [];
+    for (const client of allClients) {
+      if (clientProject.includes(client)) {
+        this._clientName = client;
+        return client;
+      }
+    }
+    // Do something here if there is no client in the list with this name
+    // actually this is kinda weird casue if there is no client in the list than there is no way to prove what the client name is
+    //if you try to do something with the client name and you cant get it something is very wrong already.
+    throw new ReferenceError("Client Name not found in list of clients");
+  }
+
+  // Validation for the constructor
+  protected static validateConstructorData({ name = "", nameArray = [], folder = null }: InitiativeParams): void {
+    if (name && nameArray.length && folder) {
+      throw new ValidationError("Initiative must be initialized with a name, nameArray, or folder");
+    }
+    // make sure only one of the three is not null
+    const countNonNull: number = [name, nameArray, folder].filter(value => value !== null).length;
+    if (countNonNull !== 1) {
+      throw new ValidationError("Too Much Data: Initiative must be constructed with either a Name, Name Array, or Folder");
+    }
+    // nameArray Validation
+    if (nameArray) {
+      for (const item of nameArray) {
+        if (item === "") {
+          throw new ValidationError("One or more elements in the nameArray are missing.");
+        }
+      }
+      if (nameArray.length != 4) {
+        throw new ValidationError("Name array does not fit the expected format");
+      }
+      if (!regex4Digits.test(nameArray[1])) {
+        throw new ValidationError("the second element in the nameArray must be 4 digits with nothing else.")
+      }
+      for (const item of nameArray) {
+        if (item === "") {
+          throw new ValidationError("One or more elements in the nameArray are missing.");
+        }
+      }
+    }
   }
 }
---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+// Folder Ids
+export const reconciliationFolderId: string = "1NNilomBvR1yumr9e89IE2qysCKLCs55O";
+export const clientFolderId: string = "1Y5CTtT86ORvnZXg-yuY6el5tWx1AbBfp";
+// Spreadsheet Ids
+export const projectDataSheetId: string = '1a7d08zpaNTUMUAa9nH10UHHIkGoBJfhVd-hRrnLM7ls';
+// TemplateIds
+export const reconciliationSheetTemplateId: string = '1pagP4j59__jDa2iB2YokYVG56RYChVOG2jUXuCVPG1c';
+export const proposalTemplateId: string = '1bTp3KyCw8MmU7WJoAVyhA_x_MPbYWTwqhXzkBM7vq20';
+export const costingSheetTemplateId: string = '1UJ5P8V92bFpJEcccCiIwAULAN5Cv_zb7YWjY38i7A9A';
+// OPD Sheed Ids (these are all inside the ProjectDataSheet)
+export const proposalsSheetId: number = 202907659;
+//Regex queries
+export const regexJobName: RegExp = /^\d{4}\s\d{4}\s.*/;
+export const regexProposalName: RegExp = /^PROPOSAL: \d{4}\s.*/;
+export const regex4Digits: RegExp = /^\d{4}/;
+export const regexProposalOpen: RegExp = /^PROPOSAL:/;
