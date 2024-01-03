c54c9613323adfeca383325559e541513bf4ece9
QVC <qnncecil@gmail.com>
2024-01-03 17:17:27 -0600

    Convert constants to script properties

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+// Everything below has be converted into a script property
+/*
 // Folder Ids
 export const reconciliationFolderId: string = "1NNilomBvR1yumr9e89IE2qysCKLCs55O";
 export const clientFolderId: string = "1Y5CTtT86ORvnZXg-yuY6el5tWx1AbBfp";
@@ -9,6 +11,8 @@ export const proposalTemplateId: string = '1bTp3KyCw8MmU7WJoAVyhA_x_MPbYWTwqhXzk
 export const costingSheetTemplateId: string = '1UJ5P8V92bFpJEcccCiIwAULAN5Cv_zb7YWjY38i7A9A';
 // OPD Sheed Ids (these are all inside the ProjectDataSheet)
 export const proposalsSheetId: number = 202907659;
+*/
+export const properties = PropertiesService.getScriptProperties();
 //Regex queries
 export const regexJobName: RegExp = /^\d{4}\s\d{4}\s.*/;
 export const regexProposalName: RegExp = /^PROPOSAL: \d{4}\s.*/;
