f0e504c09b57f210f21404b6ac863892edf29f1b
QVC <qnncecil@gmail.com>
2024-01-05 16:25:19 -0600

    Add new classes and dependencies

---
+++ src/buildins.ts
@@ -0,0 +1,14 @@
+export function onInstall(e: object): void {
+    onOpen(e);
+}
+
+export function onOpen(e: object): void {
+    // yea idk what goin on here
+    console.log(e);
+}
+
+export function onEdit(e: object): void {
+    // yea idk what goin on here
+    console.log(e);
+}
+
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,39 @@
 import { Project, Proposal } from './classes/initiatives';
 
+export interface InitEvent {
+  hostApp: string,
+  userLocale: string,
+  userTimezone: {
+    id: string,
+    offset: string
+  },
+  userCountry: string,
+  clientPlatform: string,
+  commonEventObject: {
+    hostApp: string,
+    timeZone: {
+      id: string,
+      offset: string
+    },
+    platform: string,
+    userLocale: string
+  },
+  calendar: {
+    capabilities: {
+      canSeeAttendees: boolean,
+      canSeeConferenceData: boolean
+    },
+    calendarId: string,
+    id: string,
+    organizer: {
+      email: string
+    }
+  }
+}
+
+export interface SerializedData {
+  [key: string]: string | SerializedData | SerializedData[];
+}
 export interface SerializedInitiative {
   [key: string]: string | number;
 }
@@ -7,13 +41,16 @@ export interface SerializedInitiative {
 export interface InitiativeParams {
   name?: string;
   nameArray?: string[];
-  folder?: GoogleAppsScript.Drive.Folder | null;
+  folder?: GoogleAppsScript.Drive.Folder;
 }
 
 export interface ClientParams {
   name?: string;
-  folder?: GoogleAppsScript.Drive.Folder | null;
+  folder?: GoogleAppsScript.Drive.Folder;
+}
+
+export interface BookingParams {
+  event?: InitEvent;
 }
 
 export type Initiative = Project | Proposal;
-//TODO does the initiative need to be the parent class search somehow?
