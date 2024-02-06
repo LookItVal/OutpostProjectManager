76d9182f33accaf4fdf759d5c1f2f07af7988bd1
QVC <qnncecil@gmail.com>
2024-02-06 12:21:01 -0600

    updated bechmarking system on server side for request handlers

--- src/opd/requestHandlers.ts
+++ src/opd/requestHandlers.ts
@@ -1,12 +1,12 @@
 import { Initiative, Project, Proposal } from '../classes/initiatives';
-import { SerializedData, ProposalNameArray, InitiativeParams } from '../interfaces';
+import { SerializedData, ProposalNameArray, InitiativeParams, unknownFunction } from '../interfaces';
 import { ValidationError } from '../classes/errors';
 import { properties, spreadsheet, version } from '../constants';
 import { openChangelogAsModalDialogue } from '../changelog';
-import { benchmark, verboseLog } from '../utilities';
 import { User } from '../classes/user';
 
 interface RequestHandlersExports {
+  [key: string]: unknownFunction | unknown;
   Project: typeof Project;
   Proposal: typeof Proposal;
   ValidationError: typeof ValidationError;
@@ -15,155 +15,67 @@ interface RequestHandlersExports {
   spreadsheet: typeof spreadsheet;
   version: typeof version;
   openChangelogAsModalDialogue: typeof openChangelogAsModalDialogue;
-  benchmark: typeof benchmark;
-  verboseLog: typeof verboseLog;
 }
 declare const exports: RequestHandlersExports;
+let current_benchmark: {[key: string]: number} = {};
 
 export function jumpToProposal(): void {
-  const benchmarked = exports.benchmark( function jumpToProposal() {
-    const spreadsheet = exports.spreadsheet as GoogleAppsScript.Spreadsheet.Spreadsheet;
-    const sheet = exports.Proposal.proposalSheet;
-    if (!spreadsheet) {
-      throw new ReferenceError('Spreadsheet is not defined');
-    }
-    if (!sheet) {
-      throw new ReferenceError('Sheet is not defined');
-    }
-    spreadsheet.setActiveSheet(sheet as GoogleAppsScript.Spreadsheet.Sheet);
-    const lastRow = sheet.getLastRow();
-    sheet.getRange(`A${lastRow}`).activate();
-  });
-  benchmarked();
+  const spreadsheet = exports.spreadsheet as GoogleAppsScript.Spreadsheet.Spreadsheet;
+  const sheet = exports.Proposal.proposalSheet;
+  if (!spreadsheet) {
+    throw new ReferenceError('Spreadsheet is not defined');
+  }
+  if (!sheet) {
+    throw new ReferenceError('Sheet is not defined');
+  }
+  spreadsheet.setActiveSheet(sheet as GoogleAppsScript.Spreadsheet.Sheet);
+  const lastRow = sheet.getLastRow();
+  sheet.getRange(`A${lastRow}`).activate();
 }
 
 
 export function jumpToProject(): void {
-  const benchmarked = exports.benchmark( function jumpToProject() {
-    const spreadsheet = exports.spreadsheet as GoogleAppsScript.Spreadsheet.Spreadsheet;
-    const sheet = exports.Project.recentSheet;
-    spreadsheet.setActiveSheet(sheet);
-    sheet.setActiveRange(sheet.getRange(`A${exports.Project.recentRow}`));
-  });
-  benchmarked();
+  const spreadsheet = exports.spreadsheet as GoogleAppsScript.Spreadsheet.Spreadsheet;
+  const sheet = exports.Project.recentSheet;
+  spreadsheet.setActiveSheet(sheet);
+  sheet.setActiveRange(sheet.getRange(`A${exports.Project.recentRow}`));
 }
 
 export function getInitiative(): SerializedData {
-  const benchmarked = exports.benchmark( function getInitiative(): SerializedData {
-    try {
-      const lucky_charms = Project.getInitiative().serialize();
-      console.log('THIS IS THE LAST PART OF THE BACKEND FUNCTION', lucky_charms);
-      return lucky_charms;
-    } catch (e: unknown) {
-      if (e instanceof ValidationError) {
-        console.error(e.message);
-        return {'title': e.message.split(':')[0]} as SerializedData;
-      }
-      console.error(e);
-      return {'title': 'A fatal error has occured.'} as SerializedData;
+  try {
+    const lucky_charms = Project.getInitiative().serialize();
+    console.log('THIS IS THE LAST PART OF THE BACKEND FUNCTION', lucky_charms);
+    return lucky_charms;
+  } catch (e: unknown) {
+    if (e instanceof ValidationError) {
+      console.error(e.message);
+      return {'title': e.message.split(':')[0]} as SerializedData;
     }
-  });
-  return benchmarked() as SerializedData;
-}
-
-export function requestProposalGeneration(): boolean {
-  const benchmarked = exports.benchmark( function requestProposalGeneration(): boolean {
-    const proposal = Proposal.getProposal();
-    if (proposal.type !== 'PROPOSAL') {
-      throw new ValidationError('Proposal type is not set to proposal.');
-    }
-    const ui = SpreadsheetApp.getUi();
-    const response = ui.alert(
-      'Generate Proposal?',
-      `Are you sure you want to generate a proposal in the ${proposal.clientName}? folder called ${proposal.title}?`,
-      ui.ButtonSet.YES_NO);
-    if (response === ui.Button.YES) {
-      return true;
-    }
-    return false;
-  });
-  return benchmarked() as boolean;
+    console.error(e);
+    return {'title': 'A fatal error has occured.'} as SerializedData;
+  }
 }
 
 export function generateProposal(nameArray: ProposalNameArray): void {
-  const benchmarked = exports.benchmark( function generateProposal(nameArray): void {
-    Proposal.getProposal({nameArray} as InitiativeParams).generateProposal();
-  });
-  benchmarked(nameArray);
-}
-
-export function requestProposalAccept(): boolean {
-  const proposal = Proposal.getProposal();
-  if (proposal.type !== 'PROPOSAL') {
-    throw new ValidationError('Proposal type is not set to proposal.');
-  }
-  if (proposal.status !== 'ACTIVE') {
-    throw new ValidationError('Proposal status is not set to active.');
-  }
-  const ui = SpreadsheetApp.getUi();
-  const response = ui.alert(
-    'Accept Proposal?',
-    `Are you sure you want to accept the proposal ${proposal.title} into a full project?`,
-    ui.ButtonSet.YES_NO);
-  if (response === ui.Button.YES) {
-    return true;
-  }
-  return false;
+  Proposal.getProposal({nameArray} as InitiativeParams).generateProposal();
 }
 
 export function acceptProposal(nameArray: ProposalNameArray): void {
-  const benchmarked = exports.benchmark( function acceptProposal(nameArray): void {
-    Proposal.getProposal({nameArray} as InitiativeParams).acceptProposal();
-    jumpToProject();
-  });
-  benchmarked(nameArray);
-}
-
-export function requestJobGeneration(): boolean {
-  const project = Project.getProject();
-  if (project.type !== 'PROJECT') {
-    throw new ValidationError('Project type is not set to project.');
-  }
-  const ui = SpreadsheetApp.getUi();
-  const response = ui.alert(
-    'Generate Job?',
-    `Are you sure you want to generate a job in the ${project.clientName}? folder called ${project.title}?`,
-    ui.ButtonSet.YES_NO);
-  if (response === ui.Button.YES) {
-    return true;
-  }
-  return false;
+  Proposal.getProposal({nameArray} as InitiativeParams).acceptProposal();
+  jumpToProject();
 }
 
 export function generateJob(nameArray: ProposalNameArray): void {
-  const benchmarked = exports.benchmark( function generateJob(nameArray): void {
-    Project.getProject({nameArray} as InitiativeParams).generateProject();
-  });
-  benchmarked(nameArray);
+  Project.getProject({nameArray} as InitiativeParams).generateProject();
 }
 
 export function openSheetChangelog(): void {
-  const benchmarked = exports.benchmark( function openSheetChangelog(): void {
-    const ui = SpreadsheetApp.getUi();
-    ui.showModalDialog(exports.openChangelogAsModalDialogue(), 'Changelog');
-  });
-  benchmarked();
+  const ui = SpreadsheetApp.getUi();
+  ui.showModalDialog(exports.openChangelogAsModalDialogue(), 'Changelog');
 }
 
 export function initConstants(): SerializedData {
   return {version: exports.version};
-}
-
-export function requestBenchmark(count = 1): boolean {
-  const ui = SpreadsheetApp.getUi();
-  const response = ui.alert(
-    'Benchmark',
-    `Would you like to run ${count} benchmark tests?`,
-    ui.ButtonSet.YES_NO);
-  if (response === ui.Button.YES) {
-    return true;
-  }
-  return false;
 }
 
 //////////////////////////////////
@@ -209,6 +121,8 @@ export function deleteClientFiles(): void {
   initiative.client.deleteClientFiles();
 }
 
+// add function to delete the text in the spreadsheet but make sure it only does if its thee test client weve been playing with.
+
 export function selectEmptyProposal(): void {
   const sheet = SpreadsheetApp.getActiveSheet();
   const row = sheet.getLastRow()+1;
@@ -233,4 +147,27 @@ export function deleteProposalFiles(): void {
     throw new Error('You are not authorized to perform this action.');
   }
   proposal.deleteFiles();
+}
+
+// will only run a function that has been exported.
+export function benchmark(function_name: string, ...args: unknown[]): unknown {
+  if (function_name === 'clear') {
+    current_benchmark = {};
+    return;
+  }
+  if (function_name === 'get') {
+    return current_benchmark;
+  }
+  //if function_name is not in exports, throw an error
+  if (!(function_name in exports)) {
+    throw new Error('Function not found');
+  }
+  if (typeof exports[function_name] !== 'function') {
+    throw new Error('Function not found');
+  }
+  const start = performance.now();
+  const result = (exports[function_name] as unknownFunction)(...args);
+  const end = performance.now();
+  current_benchmark[function_name] = end - start;
+  return result;
 }
