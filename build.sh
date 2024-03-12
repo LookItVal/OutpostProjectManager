cb69dc15912ff566c211bb26388c3608746f963a
QVC <qnncecil@gmail.com>
2024-03-12 14:50:51 -0500

    Add build script for deploying Apps Script code in other projectts

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/bin/zsh
+
+args=("$@")
+
+# Check if to make sure only 0 or 1 arg was provided
+if [ $# -gt 1 ]; then
+  echo "Too many arguments provided. Only 0 or 1 argument is allowed."
+  exit 1
+fi
+
+# Init empty directory variable
+directory=""
+
+if [ $# -eq 1 ]; then
+  # Check if the first argument is a valid directory
+  if [ -d $1 ]; then
+    directory=$1
+  else
+    echo "The directory $1 does not exist."
+    exit 1
+  fi
+fi
+
+if [ $# -eq 0 ]; then
+  clasp push
+  exit 0
+fi
+
+# Rename the current appsscript.json file to appsscript.json.bak
+mv appsscript.json appsscript.json.bak
+
+# Create a new symlink from the appsscript.json file in the directory to the root
+# Check to make sure appscript file exists in the directory
+if [ -f $directory/appsscript.json ]; then
+  ln -s $directory/appsscript.json appsscript.json
+else
+  echo "The appsscript.json file does not exist in the directory $directory."
+  exit 1
+fi
+
+cd $directory
+clasp push
+cd ..
+
+# Remove the symlink and rename the appsscript.json.bak file back to appsscript.json
+rm appsscript.json
+mv appsscript.json.bak appsscript.json
+exit 0
