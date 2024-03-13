65999401ce19cbed5ae6f1c45de084dba5c7aeac
QVC <qnncecil@gmail.com>
2024-03-13 12:32:13 -0500

    Refactor code and add autofill functionality

--- proposal_document/constants.ts
+++ proposal_document/constants.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace Constants {
-  export const originalTerms = 'Dates are firm with';
-  export const originalTermsTerminator = /^[\\s]*Orders or project additions/;
+  export const ORIGINAL_TERMS = 'Dates are firm with';
+  export const ORIGINAL_TERMS_TERMINATOR = /^[\\s]*Orders or project additions/;
 
-  export const terms100 = 'Dates are firm with 100% payment due at the time of booking.';
-  export const terms50 = 'Dates are firm with 50% payment due at the time of booking, 50% due at the time of final delivery.';
-  export const terms35 = 'Dates are firm with 35% payment due at the time of booking, 35% due at the midpoint of the project, and 50% due at the time of final delivery.';
+  export const TERMS_100 = 'Dates are firm with 100% payment due at the time of booking.';
+  export const TERMS_50 = 'Dates are firm with 50% payment due at the time of booking, 50% due at the time of final delivery.';
+  export const TERMS_35 = 'Dates are firm with 35% payment due at the time of booking, 35% due at the midpoint of the project, and 50% due at the time of final delivery.';
 }
