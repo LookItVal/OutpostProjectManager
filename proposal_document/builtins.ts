7bd8c7de4692463b2461dc52b52a9b9011578512
QVC <qnncecil@gmail.com>
2024-03-13 15:30:04 -0500

    Add named range functionality

--- proposal_document/builtins.ts
+++ proposal_document/builtins.ts
@@ -1,6 +1,4 @@
 export function onOpen(e: GoogleAppsScript.Events.DocsOnOpen): void {
-  console.log(e);
-  // Maybe you can get the ui from e, check the logs.
   const ui = DocumentApp.getUi();
   ui.createMenu('Outpost Project Manager')
     .addItem('Set Header Title', 'Autofill.setHeaderTitle')
@@ -11,6 +9,30 @@ export function onOpen(e: GoogleAppsScript.Events.DocsOnOpen): void {
     .addSeparator()
     .addItem('Name Range', 'NamedRange.nameRange')
     .addItem('List Named Ranges', 'NamedRange.listNamedRanges')
+    .addItem('Remove Named Range', 'NamedRange.removeNamedRange')
+    .addItem('Named Range Details', 'NamedRange.namedRangeDetails')
     .addToUi();
+
+  if (e.source.getName() === 'Proposal Template') {
+    return;
+  }
+  let result = ui.alert('Autofill', 'Permission to autofill content?', ui.ButtonSet.YES_NO);
+  if (result !== ui.Button.YES) {
+    return;
+  }
   Autofill.onOpen(e);
+  result = ui.alert('Terms 50/50%', 'Would you like to autofill the terms based on the following:\nDates are firm with 50% payment due at the time of booking, 50% due at the time of final delivery.', ui.ButtonSet.YES_NO);
+  if (result === ui.Button.YES) {
+    Autofill.setTerms50(e);
+    return;
+  }
+  result = ui.alert('Terms 100% Upfront', 'Would you like to autofill the terms based on the following:\nDates are firm with 100% payment due at the time of booking.', ui.ButtonSet.YES_NO);
+  if (result === ui.Button.YES) {
+    Autofill.setTerms100(e);
+    return;
+  }
+  result = ui.alert('Terms 35/35/30%', 'Would you like to autofill the terms based on the following:\nDates are firm with 35% payment due at the time of booking, 35% due at the midpoint of the project, and 50% due at the time of final delivery.', ui.ButtonSet.YES_NO);
+  if (result === ui.Button.YES) {
+    Autofill.setTerms35(e);
+  }
 }
