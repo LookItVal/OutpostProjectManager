1fd08e9e65b19e6fbe2f5a13aed169fc89041cb0
QVC <qnncecil@gmail.com>
2024-03-14 10:56:36 -0500

    fixed error in typying

--- proposal_document/autofill.ts
+++ proposal_document/autofill.ts
@@ -1,6 +1,14 @@
+interface DocEvent {
+  source: GoogleAppsScript.Document.Document;
+  user?: {
+    email: string;
+    nickname: string;
+  };
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace Autofill {
-  export function onOpen(e: GoogleAppsScript.Events.DocsOnOpen): void {
+  export function onOpen(e: DocEvent): void {
     console.log(e);
     const doc = e.source as GoogleAppsScript.Document.Document;
     if (doc.getNamedRanges('projectNameHeader').length !== 0) {
@@ -8,13 +16,13 @@ namespace Autofill {
     }
   }
 
-  export function initializeDocument(e: GoogleAppsScript.Events.DocsOnOpen): void {
+  export function initializeDocument(e: DocEvent): void {
     setHeaderTitle(e);
     fillEmail(e);
     fillName(e);
   }
 
-  export function setHeaderTitle(e?: GoogleAppsScript.Events.DocsOnOpen): void {
+  export function setHeaderTitle(e?: DocEvent): void {
     const doc = e?.source ?? DocumentApp.getActiveDocument();
     const header = doc.getNamedRanges('projectNameHeader')[0]?.getRange();
     const name = doc.getName();
@@ -26,7 +34,7 @@ namespace Autofill {
     doc.getNamedRanges('projectNameHeader')[0].remove();
   }
 
-  export function setTerms(newTerms: string, e?: GoogleAppsScript.Events.DocsOnOpen): void {
+  export function setTerms(newTerms: string, e?: DocEvent): void {
     const doc = e?.source ?? DocumentApp.getActiveDocument();
     const termsRange = doc.getNamedRanges('terms')[0]?.getRange();
     const textRange = termsRange.getRangeElements()[0].getElement();
@@ -48,26 +56,17 @@ namespace Autofill {
     doc.addNamedRange('terms', newRange.build());
   }
 
-  export function setTerms100(e?: GoogleAppsScript.Events.DocsOnOpen): void {
+  export function setTerms100(e?: DocEvent): void {
     setTerms(Constants.TERMS_100, e);
   }
 
-  export function setTerms50(e?: GoogleAppsScript.Events.DocsOnOpen): void {
+  export function setTerms50(e?: DocEvent): void {
     setTerms(Constants.TERMS_50, e);
   }
 
-  export function setTerms35(e?: GoogleAppsScript.Events.DocsOnOpen): void {
+  export function setTerms35(e?: DocEvent): void {
     setTerms(Constants.TERMS_35, e);
   }
-
-  interface DocEvent {
-    source: GoogleAppsScript.Document.Document;
-    user?: {
-      email: string;
-      nickname: string;
-    };
-  }
-
 
   export function fillName(e: DocEvent): void {
     const doc = e.source ?? DocumentApp.getActiveDocument();
@@ -76,7 +75,6 @@ namespace Autofill {
     nameRange.getRangeElements()[0].getElement().asText().setText(`Prepared by: ${name.charAt(0).toUpperCase()}${name.slice(1)} - Outpost Worldwide, Inc.`);
     nameRange.getRangeElements()[0].getElement().asText().setForegroundColor('#000000');
   }
-
 
   export function fillEmail(e: DocEvent): void {
     const doc = e.source;
