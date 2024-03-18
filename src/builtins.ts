1c05d603b39f6262f3fba385f51c58da55e991bf
QVC <qnncecil@gmail.com>
2024-03-18 09:44:23 -0500

    Auto add theme to spreadsheets (you think this worked?)

--- src/builtins.ts
+++ src/builtins.ts
@@ -1,10 +1,25 @@
-export function onInstall(e: object): void {
+import { Colors } from './constants';
+
+export function onInstall(e: GoogleAppsScript.Events.SheetsOnOpen): void {
   onOpen(e);
 }
 
-export function onOpen(e: object): void {
+export function onOpen(e: GoogleAppsScript.Events.SheetsOnOpen): void {
   // yea idk what goin on here
-  console.log(e);
+  console.log('THIS IS THE CUSTOM ONE');
+  const theme = e.source.getSpreadsheetTheme();
+  console.log(theme?.getThemeColors());
+  theme?.setConcreteColor(SpreadsheetApp.ThemeColorType.TEXT, Colors.newColor(Colors.textColor));
+  theme?.setConcreteColor(SpreadsheetApp.ThemeColorType.BACKGROUND, Colors.newColor(Colors.backgroundColor));
+  theme?.setConcreteColor(SpreadsheetApp.ThemeColorType.HYPERLINK, Colors.newColor(Colors.hyperlinkColor));
+  theme?.setConcreteColor(SpreadsheetApp.ThemeColorType.ACCENT1, Colors.newColor(Colors.accent1));
+  theme?.setConcreteColor(SpreadsheetApp.ThemeColorType.ACCENT2, Colors.newColor(Colors.accent2));
+  theme?.setConcreteColor(SpreadsheetApp.ThemeColorType.ACCENT3, Colors.newColor(Colors.accent3));
+  theme?.setConcreteColor(SpreadsheetApp.ThemeColorType.ACCENT4, Colors.newColor(Colors.accent4));
+  theme?.setConcreteColor(SpreadsheetApp.ThemeColorType.ACCENT5, Colors.newColor(Colors.accent5));
+  theme?.setConcreteColor(SpreadsheetApp.ThemeColorType.ACCENT6, Colors.newColor(Colors.accent6));
+  theme?.setFontFamily(Colors.fontFamily);
+  console.log('completed');
 }
 
 export function onEdit(e: object): void {
