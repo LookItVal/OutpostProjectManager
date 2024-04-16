f962c1324027386ef8e097f6651b1505738669a9
QVC <qnncecil@gmail.com>
2024-04-16 12:53:47 -0500

    added namespaces to constants. did not relink other files.

--- src/constants.ts
+++ src/constants.ts
@@ -1,95 +1,111 @@
-// State
-export const spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet | null  = SpreadsheetApp.getActiveSpreadsheet();
+let _properties: GoogleAppsScript.Properties.Properties | undefined = undefined;
+let _spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet| undefined = undefined;
 
-export const properties = PropertiesService.getScriptProperties();
-//Regex queries
-export const regexJobName: RegExp = /^\d{4}\s\d{4}\s.*/;
-export const regexProposalName: RegExp = /^PROPOSAL: \d{4}\s.*/;
-export const regex4Digits: RegExp = /^\d{4}/;
-export const regexProposalOpen: RegExp = /^PROPOSAL:/;
-export const regexPullDigits: RegExp = /\d+/g;
-
-//Changelog
-export const version = '1.2.6';
-export const changelog = {
-  1: {
-    2: [
-      'Shiny Things Update',
-      {
-        7: [
-          ['Fixed problem with finding the new sheet', 'Bug Fix'],
-          ['Added new heartbeat to the frontend to check for version number and connection status', 'New Feature'],
-          ['Connection now refreshes when the heartbeat is lost', 'New Feature'],
-          ['Now checks to see if the selection has changed', 'New Feature'],
-          ['Changed constants to cache value and only call when called to fix onOpen bug', 'Bug Fix'],
-        ],
-        6: [
-          ['Made new logo to make app more visable on white backgrounds', 'UI Change'],
-          ['Made new style guide for sheets', 'New Feature'],
-          ['Added Injection of style guide to sheets', 'New Feature'],
-          ['Added button to open the OPD from the calendar', 'New Feature'],
-          ['Added button make new job costing from the project stage', 'New Feature']
-        ],
-        5: [
-          ['Added autofill for project title in Reconciliation Sheet', 'New Feature'],
-          ['Added autofill for project title in Costing Sheet', 'New Feature'],
-          ['Fixed bug on copying and pasting in Costing Sheet', 'Bug Fix'],
-          ['Changed initialization script to track subscript directories', 'Maintaniance']
-        ],
-        4: [
-          ['Added autofill function for project title and user info', 'New Feature'],
-          ['Added autofill prompts to the openening of the proposal document for the first time', 'New Feature'],
-          ['Added new build system for bound scripts', 'Maintaniance'],
-          ['Added new initializations script for bound scripts', 'Maintaniance']
-        ],
-        3: [
-          ['Added Folder button to calendar sidebar', 'New Feature'],
-          ['Added Proposal and Costing sheet buttons to sidebar', 'New Feature'],
-          ['Made buttons only show up if their information was found', 'Bug Fix'],
-          ['Changed wording on some buttons', 'UI Change'],
-          ['Added buttons to proposal document to autofill title and terms', 'New Feature']
-        ],
+export namespace Constants {
+  export class Meta {
+    public version = '1.2.7';
+    public changelog = {
+      1: {
         2: [
-          ['Cleaned up log statements', 'Maintaniance'],
-          ['Managed Git Repo', 'Maintaniance'],
-          ['Added Auto-filling for Client Names in the Outpost Project Database', 'New Feature'],
-          ['Added Animation to Projects and Proposals Buttons', 'UI Change'],
-          ['Cleaned Up Client Folder Structure', 'Maintaniance'],
-          ['Added Cursor Pointing to buttons in the HTML stylesheet', 'UI Change'],
-          ['Added Changelog to the calendar sidebar', 'UI Change'],
-          ['Added Changelog to the project sidebar', 'UI Change']
-        ],
-        1: [
-          ['Stopped text from flashing on project generation', 'Bug Fix'],
-          ['Disabled Load Button when loading new content', 'Bug Fix'],
-          ['Reorganized folder structure', 'Maintaniance']
-        ],
-        0: [
-          ['Added Changelog', 'New Feature'],
-          ['Fully Converted Codebase Into Typescript.', 'Maintaniance'],
-          ['Began Readme and some documentation.', 'Maintaniance'],
-          ['Added Better organization to frontend, with bigger text and icons with subtle animations.', 'UI Change'],
-          ['Prepared the code base for more rapid changes.', 'Maintaniance']
+          'Shiny Things Update',
+          {
+            7: [
+              ['Fixed problem with finding the new sheet', 'Bug Fix'],
+              ['Added new heartbeat to the frontend to check for version number and connection status', 'New Feature'],
+              ['Connection now refreshes when the heartbeat is lost', 'New Feature'],
+              ['Now checks to see if the selection has changed', 'New Feature'],
+              ['Changed constants to cache value and only call when called to fix onOpen bug', 'Bug Fix'],
+            ],
+            6: [
+              ['Made new logo to make app more visable on white backgrounds', 'UI Change'],
+              ['Made new style guide for sheets', 'New Feature'],
+              ['Added Injection of style guide to sheets', 'New Feature'],
+              ['Added button to open the OPD from the calendar', 'New Feature'],
+              ['Added button make new job costing from the project stage', 'New Feature']
+            ],
+            5: [
+              ['Added autofill for project title in Reconciliation Sheet', 'New Feature'],
+              ['Added autofill for project title in Costing Sheet', 'New Feature'],
+              ['Fixed bug on copying and pasting in Costing Sheet', 'Bug Fix'],
+              ['Changed initialization script to track subscript directories', 'Maintaniance']
+            ],
+            4: [
+              ['Added autofill function for project title and user info', 'New Feature'],
+              ['Added autofill prompts to the openening of the proposal document for the first time', 'New Feature'],
+              ['Added new build system for bound scripts', 'Maintaniance'],
+              ['Added new initializations script for bound scripts', 'Maintaniance']
+            ],
+            3: [
+              ['Added Folder button to calendar sidebar', 'New Feature'],
+              ['Added Proposal and Costing sheet buttons to sidebar', 'New Feature'],
+              ['Made buttons only show up if their information was found', 'Bug Fix'],
+              ['Changed wording on some buttons', 'UI Change'],
+              ['Added buttons to proposal document to autofill title and terms', 'New Feature']
+            ],
+            2: [
+              ['Cleaned up log statements', 'Maintaniance'],
+              ['Managed Git Repo', 'Maintaniance'],
+              ['Added Auto-filling for Client Names in the Outpost Project Database', 'New Feature'],
+              ['Added Animation to Projects and Proposals Buttons', 'UI Change'],
+              ['Cleaned Up Client Folder Structure', 'Maintaniance'],
+              ['Added Cursor Pointing to buttons in the HTML stylesheet', 'UI Change'],
+              ['Added Changelog to the calendar sidebar', 'UI Change'],
+              ['Added Changelog to the project sidebar', 'UI Change']
+            ],
+            1: [
+              ['Stopped text from flashing on project generation', 'Bug Fix'],
+              ['Disabled Load Button when loading new content', 'Bug Fix'],
+              ['Reorganized folder structure', 'Maintaniance']
+            ],
+            0: [
+              ['Added Changelog', 'New Feature'],
+              ['Fully Converted Codebase Into Typescript.', 'Maintaniance'],
+              ['Began Readme and some documentation.', 'Maintaniance'],
+              ['Added Better organization to frontend, with bigger text and icons with subtle animations.', 'UI Change'],
+              ['Prepared the code base for more rapid changes.', 'Maintaniance']
+            ]
+          }
         ]
       }
-    ]
-  }
-};
+    };
+     
+    public get properties(): GoogleAppsScript.Properties.Properties {
+      if (_properties) {
+        return _properties;
+      }
+      return _properties = PropertiesService.getScriptProperties();
+    }
 
-//Colors
-export namespace Colors {
-  export function newColor(color: string): GoogleAppsScript.Spreadsheet.Color {
-    return SpreadsheetApp.newColor().setRgbColor(color).build();
+    public get spreadsheet(): GoogleAppsScript.Spreadsheet.Spreadsheet {
+      if (_spreadsheet) {
+        return _spreadsheet;
+      }
+      return _spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    }
   }
-  export const textColor: string = '#000000';
-  export const backgroundColor: string = '#F4FAFF';
-  export const hyperlinkColor: string = '#3F72A0';
-  export const fontFamily: string = 'Arial';
-  export const headingFontFamily: string = 'Century Gothic';
-  export const accent1: string = '#1b8acf';
-  export const accent2: string = '#80bbff';
-  export const accent3: string = '#9fa6b3';
-  export const accent4: string = '#bdc6d4';
-  export const accent5: string = '#dbe5f6';
-  export const accent6: string = '#e3edff';
-}  
+
+  export namespace Regex {
+    export const jobName: RegExp = /^\d{4}\s\d{4}\s.*/;
+    export const isproposalName: RegExp = /^PROPOSAL: \d{4}\s.*/;
+    export const is4Digits: RegExp = /^\d{4}/;
+    export const regexProposalOpen: RegExp = /^PROPOSAL:/;
+    export const regexPullDigits: RegExp = /\d+/g;
+  }
+  
+  export namespace Colors {
+    export function newColor(color: string): GoogleAppsScript.Spreadsheet.Color {
+      return SpreadsheetApp.newColor().setRgbColor(color).build();
+    }
+    export const textColor: string = '#000000';
+    export const backgroundColor: string = '#F4FAFF';
+    export const hyperlinkColor: string = '#3F72A0';
+    export const fontFamily: string = 'Arial';
+    export const headingFontFamily: string = 'Century Gothic';
+    export const accent1: string = '#1b8acf';
+    export const accent2: string = '#80bbff';
+    export const accent3: string = '#9fa6b3';
+    export const accent4: string = '#bdc6d4';
+    export const accent5: string = '#dbe5f6';
+    export const accent6: string = '#e3edff';
+  }  
+}
