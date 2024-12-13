6dcbd68645986087e7bdf8420101c605d9bec31b
Quinn Valencia Cecil <qnncecil@gmail.com>
2024-12-13 13:03:38 -0600

    Add JSDoc comments to User class for improved documentation

--- src/classes/user.ts
+++ src/classes/user.ts
@@ -1,9 +1,32 @@
+/**
+ * Basic user class for getting user information.
+ * @module src/classes/user
+ */
+
 import { State } from '../constants';
 
-
+/**
+ * The User static class for getting information about the active user.
+ * @static
+ * @class User
+ * @memberof src/classes/user
+ * @exports User
+ * @property {string} email - The user's email address.
+ * @property {string} fullName - The user's full name.
+ * @property {boolean} isAdmin - Whether the user is an administrator.
+ * @property {boolean} isDeveloper - Whether the user is a developer.
+ */
 export class User {
+  /** Why are you doing this? do not do this. This is a STATIC class. */
   constructor() {}
 
+  /**
+   * Get the active user's email address.
+   * @static
+   * @property {string} email
+   * @returns {string} The user's email address.
+   * @throws {Error} If the user is not found.
+   */
   static get email(): string {
     const user = People.People?.get('people/me', {personFields: 'emailAddresses'});
     if (!user) {
@@ -16,6 +39,13 @@ export class User {
     return emailAddresses[0].value as string;
   }
 
+  /**
+   * Get the active user's full name.
+   * @static
+   * @property {string} fullName
+   * @returns {string} The user's full name.
+   * @throws {Error} If the user is not found.
+   */
   static get fullName(): string {
     const user = People.People?.get('people/me', {personFields: 'names'});
     if (!user) {
@@ -32,6 +62,12 @@ export class User {
     return name;
   }
 
+  /**
+   * Check if the active user is an administrator.
+   * @static
+   * @property {boolean} isAdmin
+   * @returns {boolean} Whether the user is an administrator.
+   */
   static get isAdmin(): boolean {
     const email = User.email;
     let isAdmin = false;
@@ -43,6 +79,12 @@ export class User {
     return isAdmin;
   }
 
+  /**
+   * Check if the active user is a developer.
+   * @static
+   * @property {boolean} isDeveloper
+   * @returns {boolean} Whether the user is a developer.
+   */
   static get isDeveloper(): boolean {
     const email = User.email;
     let isDeveloper = false;
